3f9d72e1f2b70724f8c5e1309f2f59d1f0f72d0c
ZimConnect <berryzcarwash@gmail.com>
2026-08-30 22:31:20 +0200

    Change home screen to LoginScreen and update theme
    Updated the main app to use LoginScreen as the home screen and simplified the theme configuration.

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,22 @@
 import 'package:flutter/material.dart';
-import 'package:shopsmart_zw/screens/home_screen.dart';
 import 'package:shopsmart_zw/services/db_service.dart';
+import 'package:shopsmart_zw/screens/login_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await DBService.instance.database; // init DB
+  await DBService.instance.database;
   runApp(const ShopSmartApp());
 }
 
 class ShopSmartApp extends StatelessWidget {
   const ShopSmartApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'ShopSmart ZW',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primaryColor: const Color(0xFF006400), // Zim green
-        colorScheme: ColorScheme.fromSeed(seedColor: Color(0xFF006400)),
-        useMaterial3: true,
-        textTheme: const TextTheme(bodyLarge: TextStyle(fontSize: 16)),
-      ),
-      home: const HomeScreen(),
+      theme: ThemeData(primarySwatch: Colors.green, useMaterial3: true),
+      home: const LoginScreen(), // Start with PIN
     );
   }
 }
